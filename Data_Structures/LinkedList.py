75367c12de16c30cda1b3dc890ce5648396029e4
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-04-30 12:48:58 -0500

    Update LinkedList.py

--- Data_Structures/LinkedList.py
+++ Data_Structures/LinkedList.py
@@ -89,9 +89,21 @@ class LinkedList(Generic[T]):
     def isEmpty(self):
         return self.head == None
 
-    def find(self, keyFind):
-        flag = False
-        return flag
+    def find(self, node_To_Find):
+        return not self.findPosition(node_To_Find)==-1
+
+    def findPosition(self, node_To_Find):
+        if not self.isEmpty():
+            ptr = self.head
+            i=0
+            while (not ptr is None):
+                if ptr.key==node_To_Find.key:
+                    return i
+                ptr = ptr.next
+                i+=1
+            return -1
+        else:
+            raise Exception("List is empty")
 
     def size(self):
         headRef = self.head
@@ -104,8 +116,7 @@ class LinkedList(Generic[T]):
                 headRef = headRef.next
             return size + 1
 
-    def insert(self,index,value):
-        newNode = Node(value)
+    def insert(self,index,newNode):
         if index==0:
             self.pushFront(newNode)
         else:
@@ -120,28 +131,50 @@ class LinkedList(Generic[T]):
             ptr.next = newNode
 
 
-    def insertOrder(self, value):
-        new_node = Node(value)
-        if self.isEmpty() or value < self.head.key:
+    def insertOrder(self, new_node):
+        if self.isEmpty() or new_node.key < self.head.key:
             new_node.next = self.head
             self.head = new_node
         else:
             ptr = self.head
-            while ptr.next is not None and ptr.next.key < value:
+            while ptr.next is not None and ptr.next.key < new_node.key:
                 ptr = ptr.next
             new_node.next = ptr.next
             ptr.next = new_node
 
-    def delete(self, data):
+    def delete(self, node_bye):
         if self.isEmpty():
             raise Exception("Empty List")
-        elif self.head.key == data:
+        elif self.head.key == node_bye.key:
             self.head = self.head.next
         else:
             curr_node = self.head
             prev_node = None
-            while curr_node and curr_node.key != data:
+            while curr_node and curr_node.key != node_bye.key:
                 prev_node = curr_node
                 curr_node = curr_node.next
             if curr_node:
                 prev_node.next = curr_node.next
+
+    def sort(self):
+        if not self.isEmpty():
+            linkedListSorted = LinkedList()
+            ptr_Or = self.head
+            while not (ptr_Or is None):
+                ptr_nw = Node(ptr_Or.key)
+                linkedListSorted.insertOrder(ptr_nw)
+                ptr_Or = ptr_Or.next
+            self.head = linkedListSorted.head
+
+    def reverse(self):
+        if not self.isEmpty():
+            linkedListReversed = LinkedList()
+            ptr_Or = self.head
+            while not (ptr_Or is None):
+                linkedListReversed.pushFront(Node(ptr_Or.key))
+                ptr_Or = ptr_Or.next
+            self.head = linkedListReversed.head
+
+    def reversedSort(self):
+        self.sort()
+        self.reverse()
